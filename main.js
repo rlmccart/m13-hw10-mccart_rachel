f82e9706f7826364c6f738b2bfa356aa6410e7dc
Rachel McCart <rlmccart@ufl.edu>
2018-11-16 16:44:26 -0600

    Comment outpart 1 javascript functionality

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 //JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
-var section = document.getElementByTagName('section');
-var button = document.getElementbyTagName('button');
+//var section = document.getElementByTagName('section');
+//var button = document.getElementbyTagName('button');
 
-function slideToggle() {
-    section.classList.toggle('hide');
-};
+//function slideToggle() {
+//    section.classList.toggle('hide');
+//};
 
-for (i=0; i < button.length; i++) {
-    button[i].addEventListener('click', slideToggle);
-}; 
+//for (i=0; i < button.length; i++) {
+//    button[i].addEventListener('click', slideToggle);
+//}; 
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
